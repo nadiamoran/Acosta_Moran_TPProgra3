7a53016c0136a7e20287ab18254e4112e15e18bc
Nadia Moran <111939771+nadiamoran@users.noreply.github.com>
2026-06-23 22:51:34 -0300

    Update bienvenida.js

--- bienvenida.js
+++ bienvenida.js
@@ -1,15 +1,26 @@
-const boton = document.getElementById("btnContinuar");
+const boton =
+document.getElementById("btnContinuar");
 
 boton.addEventListener("click", () => {
 
-    const nombre = document.getElementById("nombre").value;
+    const nombre =
+    document.getElementById("nombre").value;
 
-    if (nombre === "") {
-        alert("Ingrese su nombre");
+    if(nombre.trim() === ""){
+
+        alert(
+            "Debe ingresar un nombre"
+        );
+
         return;
     }
 
-    localStorage.setItem("cliente", nombre);
+    localStorage.setItem(
+        "cliente",
+        nombre
+    );
 
-    window.location.href = "productos.html";
+    window.location.href =
+    "productos.html";
+
 });
